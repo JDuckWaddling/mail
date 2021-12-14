6c812da61c573f79b8f82a89447f106498e23656
jiwoney <jiwoney@naver.com>
2021-12-14 15:57:14 +0900

    Test

--- cron.sh
+++ cron.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
+
 	cd /Users/hwangjiwon/Desktop/daily-news-for-trainee-master/google-news  && /usr/local/bin/tsc index.ts && /usr/local/bin/node index.js
 	cd /Users/hwangjiwon/Desktop/daily-news-for-trainee-master/sbs-news && /usr/local/bin/tsc index.ts && /usr/local/bin/node index.js
